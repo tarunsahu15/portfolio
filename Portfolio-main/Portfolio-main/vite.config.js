acd84eae4ceacc996237310938a5b48d94898b5c
tarunsahu15 <148942846+tarunsahu15@users.noreply.github.com>
2024-09-01 19:45:45 +0530

    Update vite.config.js

--- Portfolio-main/Portfolio-main/vite.config.js
+++ Portfolio-main/Portfolio-main/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "/Portfolio/",
+  base : "https://portfolio-fsro.vercel.app/Portfolio/",
   css:{
     modules:{
       localsConvention: "camelCase"
